5285d4c32c2f443672eed9346d4477a52a501902
swient <B11217048@yuntech.edu.tw>
2024-12-15 02:32:35 +0800

    feat: add icon to exe file

--- ImageRecognition_Final_Project/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ ImageRecognition_Final_Project/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -297,7 +297,7 @@ namespace ImageRecognition_Final_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ImageRecognition_Final_Project;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ImageRecognition_Final_Project;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
