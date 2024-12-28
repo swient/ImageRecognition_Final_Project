b5117f4756d26861d064c570b560b2851f07805e
swient <B11217048@yuntech.edu.tw>
2024-12-29 03:46:44 +0800

    - Feat: 完成設定和關於頁面 - 完成設定頁面，包含語言選擇、主題選擇、通知開關和自動更新開關，完成關於頁面，包含專案介紹、成員資訊和 GitHub 鏈結 - Fix: 調整設定和關於頁面的圖示寬度 - 調整設定和關於頁面的圖示寬度，使其能完全顯示 - Refactor: 重新命名設定檔案 - 重新命名設定的 C# 和 XAML 檔案為 Settings.xaml 和 Settings.xaml.cs

--- ImageRecognition_Final_Project/View/About.xaml.cs
+++ ImageRecognition_Final_Project/View/About.xaml.cs
@@ -1,12 +1,51 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ImageRecognition_Final_Project
 {
-    public partial class AboutWindow : Window
+    public partial class AboutWindow : HandyControl.Controls.Window
     {
         public AboutWindow()
         {
             InitializeComponent();
         }
+
+        private void swient_Click(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/swient",
+                UseShellExecute = true
+            });
+        }
+
+        private void Famouseegg_Click(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/famouseegg",
+                UseShellExecute = true
+            });
+        }
+
+        private void Pinshao_Click(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/pinshao",
+                UseShellExecute = true
+            });
+        }
+
+        private void Project_Link_Click(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/swient/ImageRecognition_Final_Project",
+                UseShellExecute = true
+            });
+        }
+
     }
 }
---
+++ ImageRecognition_Final_Project/View/Settings.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ImageRecognition_Final_Project
+{
+    public partial class SettingsWindow : HandyControl.Controls.Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // 保存設定的邏輯
+            HandyControl.Controls.MessageBox.Show("設定已保存！");
+        }
+    }
+}
